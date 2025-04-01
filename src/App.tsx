2e492dd743bbb0bdc64dcf46e489cae803a5ef99
Ezra Evans <ezraallenevans@gmail.com>
2025-04-01 05:44:41 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+	return (
+		<>
+			<div className="bg-green text-white w-screen h-screen">
+				<h1 className="text-3xl font-sans">
+					Hello World. Its Voy!
+				</h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
