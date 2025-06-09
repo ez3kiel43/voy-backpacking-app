df92984a6471b5367362c7104b741e5c026c4480
Ezra Evans <ezraallenevans@gmail.com>
2025-06-09 03:19:25 -0400

    changed background

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -1,39 +1,16 @@
-import { Outlet, useLocation } from 'react-router';
+import { Outlet } from 'react-router';
 import Logo from './../assets/logo.svg';
-import LogoLight from './../assets/logo_light.svg';
-import { useState, useEffect } from 'react';
 
 export default function AuthLayout() {
-	const [authPg, setAuthPg] = useState(false);
-
-	let location = useLocation();
-
-	useEffect(() => {
-		if (location.pathname == '/') {
-			setAuthPg(false);
-		} else {
-			setAuthPg(true);
-		}
-	}, [location]);
-
 	return (
 		<div
-			className={`h-full transition-all duration-100 min-h-svh bg-linear-to-br ${
-				authPg
-					? 'from-[#2B4141] to-[#4c7474]'
-					: 'from-[#eceff1] to-[#2b4141]'
-			}`}
+			className={`h-full transition-all duration-100 min-h-svh bg-linear-to-br bg-[url('/voy-background.png')]`}
 		>
 			<header
-				className={` flex flex-col justify-end border-b-4 border-red gap-1.5 py-4 w-11/12 mx-auto text-sm ${
-					authPg ? 'h-1/3 text-white' : 'h-96 text-dark-green'
-				}`}
+				className={` flex flex-col justify-end border-b-4 border-red gap-1.5 py-4 w-11/12 mx-auto text-sm
+					h-1/3`}
 			>
-				<img
-					src={authPg ? LogoLight : Logo}
-					alt="Voy"
-					className="w-1/2 mx-auto"
-				/>
+				<img src={Logo} alt="Voy" className="w-1/2 mx-auto" />
 				<h2 className="font-sans italic text-xl text-center">
 					Explore New Horizons
 				</h2>
